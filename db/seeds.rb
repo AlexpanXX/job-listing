05f755a7acb66be266ebd274f5fb41a57a628c92
AlexPan <ppgod@live.cn>
2017-04-18 22:17:51 +0800

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+Job.delete_all
+Resume.delete_all
+
+User.create!(
+  email: "ppgod@live.cn",
+  password: "12345678",
+  is_admin: true
+)
+
+num = 0
+wage_lower = 5000
+wage_upper = 10000
+50.times {
+  Job.create!(
+    title: "Job " + (num += 1).to_s,
+    description: "Job " + num.to_s + " description",
+    wage_lower_bound: wage_lower += 1,
+    wage_upper_bound: wage_upper -= 1,
+    contact_email: "ppgod@live.cn",
+    is_hidden: false
+  )
+}
+
+user = User.first
+job = Job.first
+5.times {
+  Resume.create!(
+    content: "Job first resume content.",
+    user_id: user.id,
+    job_id: job.id
+  )
+}
